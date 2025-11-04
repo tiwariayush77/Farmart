913f7c0076fcc9d2aa0a2a2d74c2ca505e857d36
ayush tiwari <tiwariayush77@gmail.com>
2025-11-04 17:43:24 +0000

    Try fixing this error: `Runtime Error: Error: Missing <html> and <body>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,21 +14,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <SettingsProvider>
-      <html lang="en">
-        <head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Noto+Sans+Bengali:wght@100..900&family=Noto+Sans+Devanagari:wght@100..900&family=Noto+Sans+Gujarati:wght@100..900&family=Noto+Sans+Gurmukhi:wght@100..900&family=Noto+Sans+Kannada:wght@100..900&family=Noto+Sans+Malayalam:wght@100..900&family=Noto+Sans+Oriya:wght@100..900&family=Noto+Sans+Tamil:wght@100..900&family=Noto+Sans+Telugu:wght@100..900&display=swap"
-            rel="stylesheet"
-          />
-        </head>
-        <body className="font-body antialiased" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Noto+Sans+Bengali:wght@100..900&family=Noto+Sans+Devanagari:wght@100..900&family=Noto+Sans+Gujarati:wght@100..900&family=Noto+Sans+Gurmukhi:wght@100..900&family=Noto+Sans+Kannada:wght@100..900&family=Noto+Sans+Malayalam:wght@100..900&family=Noto+Sans+Oriya:wght@100..900&family=Noto+Sans+Tamil:wght@100..900&family=Noto+Sans+Telugu:wght@100..900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body className="font-body antialiased" suppressHydrationWarning>
+        <SettingsProvider>
           {children}
           <Toaster />
-        </body>
-      </html>
-    </SettingsProvider>
+        </SettingsProvider>
+      </body>
+    </html>
   );
 }
